0de3c3464fa05343d58c5ba777e5b8e5b361945a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ CS/DXApplication36/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using DevExpress.Xpf.Ribbon;
+using DevExpress.XtraRichEdit;
+using DevExpress.Xpf.RichEdit;
+using DevExpress.XtraRichEdit.Services;
+using DevExpress.Xpf.Core;
+
+namespace DXApplication36 {
+    public partial class MainWindow : ThemedWindow
+    {
+        public MainWindow() {
+            InitializeComponent();
+            richEditControl1.Loaded += richEditControl1_Loaded;
+        }
+        #region #replace
+        void richEditControl1_Loaded(object sender, RoutedEventArgs e) {
+            ReplaceRichEditCommandFactoryService(richEditControl1);
+        }
+        
+        void ReplaceRichEditCommandFactoryService(RichEditControl control)
+        {
+            control.ApplyTemplate();
+            IRichEditCommandFactoryService service = control.GetService<IRichEditCommandFactoryService>();
+            control.ReplaceService<IRichEditCommandFactoryService>(new CustomRichEditCommandFactoryService(control, service));
+        }
+        #endregion #replace
+    }
+
+
+
+
+
+
+
+}
---
+++ VB/DXApplication36/MainWindow.xaml.vb
@@ -0,0 +1,38 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Windows
+Imports DevExpress.Xpf.Ribbon
+Imports DevExpress.XtraRichEdit
+Imports DevExpress.Xpf.RichEdit
+Imports DevExpress.XtraRichEdit.Services
+Imports DevExpress.Xpf.Core
+
+Namespace DXApplication36
+    Partial Public Class MainWindow
+        Inherits ThemedWindow
+
+        Public Sub New()
+            InitializeComponent()
+            AddHandler richEditControl1.Loaded, AddressOf richEditControl1_Loaded
+        End Sub
+        #Region "#replace"
+        Private Sub richEditControl1_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            ReplaceRichEditCommandFactoryService(richEditControl1)
+        End Sub
+
+        Private Sub ReplaceRichEditCommandFactoryService(ByVal control As RichEditControl)
+            control.ApplyTemplate()
+            Dim service As IRichEditCommandFactoryService = control.GetService(Of IRichEditCommandFactoryService)()
+            control.ReplaceService(Of IRichEditCommandFactoryService)(New CustomRichEditCommandFactoryService(control, service))
+        End Sub
+        #End Region ' #replace
+    End Class
+
+
+
+
+
+
+
+End Namespace
